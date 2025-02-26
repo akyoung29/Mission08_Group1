826661a73aa831cefe2d5a63c0d0aaa69d654439
annay <akyoung@byu.edu>
2025-02-26 14:27:53 -0700

    Added files and created context file

--- Mission07_Group1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mission07_Group1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
